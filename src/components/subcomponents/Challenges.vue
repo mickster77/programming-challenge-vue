fefa4a0a350ff807c760c8fafd444f73787406d4
shit.mail@icloud.com <michael.d.black2@gmail.com>
2020-03-23 13:18:06 -0400

    Challenge cmpnt updt|compontized SubmissionInput

--- src/components/subcomponents/Challenges.vue
+++ src/components/subcomponents/Challenges.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <v-card color="teal" class="mb-3" v-for="(challenge, index) in Challenges" :key="index">
+    <v-card
+      color="teal"
+      class="mb-3"
+      v-for="(challenge, index) in desendingChallenges"
+      :key="index"
+    >
       <v-card-title>{{challenge.name}}</v-card-title>
       <v-card-text>
-        <p>{{challenge.prompt}}</p>
+        Challenge:
+        {{challenge.prompt}}
       </v-card-text>
-      <v-file-input show-size label="File input"></v-file-input>
+      <v-card-text>Release Date: {{challenge.release_date}}</v-card-text>
+      <v-card-text>Due Date: {{challenge.due_date}}</v-card-text>
+      <!-- <v-file-input show-size label="File input"></v-file-input> -->
+      <SubmissionInput />
     </v-card>
   </div>
 </template>
@@ -13,8 +22,10 @@
 <script>
 // import firebase from "firebase";
 import db from "@/firebase/init";
+import SubmissionInput from "@/components/subcomponents/SubmissionInput";
 
 export default {
+  components: { SubmissionInput },
   data() {
     return {
       Challenges: []
@@ -23,9 +34,9 @@ export default {
   created() {
     db.firestore()
       .collection("Challenges")
+      .orderBy("release_date")
       .onSnapshot(snapshot => {
         this.Challenges = [];
-
         snapshot.forEach(doc => {
           let challenge = doc.data();
           let id = doc.id;
@@ -33,6 +44,13 @@ export default {
           this.Challenges.push(challenge);
         });
       });
+  },
+  computed: {
+    //I use this compute property to display the challenges, since firebase return them in order of release date.  This way, the newest challenge is on top.
+    desendingChallenges() {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      return this.Challenges.reverse();
+    }
   }
 };
 </script>
---
+++ src/components/subcomponents/SubmissionInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <v-file-input show-size label="File input"></v-file-input>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
